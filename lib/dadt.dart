45b894796d60af40d7ce2d4aac64d69716ad5ebb
Paka <paka@Pakas-Mac-mini.lan>
2024-02-01 09:00:34 +1300

    sync

--- lib/dadt.dart
+++ lib/dadt.dart
@@ -3,4 +3,3 @@ library;
 export 'src/boolean.dart';
 export 'src/option.dart';
 export 'src/result.dart';
-export 'src/future_result.dart';
--- lib/src/result.dart
+++ lib/src/result.dart
@@ -1,22 +1,75 @@
-/// A sealed class representing the result of an operation, which can be either a success or a failure.
+/// A typedef representing an asynchronous result that can either be a [Success] or a [Failure].
 ///
-/// [S] represents the type of the success value, and [F] represents the type of the failure value.
-sealed class Result<S, F> {}
+/// It is defined as a [Future] containing a [Result]
+typedef AsyncResult<S, F> = Future<Result<S, F>>;
+
+/// Represents a result that can either be a [Success] or a [Failure].
+///
+/// This class is abstract and cannot be instantiated directly.
+sealed class Result<S, F> {
+  /// Creates a [Result] instance based on the value of [s].
+  ///
+  /// If [s] is not [null], returns a [Success] instance with the provided value.
+  /// Otherwise, returns a [Failure] instance with the provided failure value [f].
+  factory Result.fromNullable(S? s, F f) => s != null ? Success(s) : Null(f);
+
+  /// Considers a succesful [Result] that is null as [Failure].
+  static AsyncResult<S?, F> fromFuture<S, F>({
+    required Future<S?> Function() future,
+    required F f,
+    bool failNullValues = true,
+  }) async {
+    try {
+      final result = await future();
+      if (result == null) {
+        if (failNullValues) {
+          return Failure(f);
+        } else {
+          return Success(result);
+        }
+      } else {
+        return Success(result);
+      }
+    } catch (e, s) {
+      return Error(f, e, s);
+    }
+  }
+}
+
+class Null<S, F> implements Result<S, F> {
+  Null(this.n);
+  final F n;
+}
 
 /// Represents a successful result with a value of type [S].
 class Success<S, F> implements Result<S, F> {
-  /// Constructs a [Success] instance with the specified [success] value.
-  Success(this.success);
-  
-  /// The value representing the success result.
-  final S success;
+  /// Creates a [Success] instance with the provided value [s].
+  Success(this.s);
+
+  /// The value of the successful result.
+  final S s;
 }
 
-/// Represents a failure result with a value of type [F].
+/// Represents a failed result with a failure value of type [F].
 class Failure<S, F> implements Result<S, F> {
-  /// Constructs a [Failure] instance with the specified [failure] value.
-  Failure(this.failure);
-  
-  /// The value representing the failure result.
-  final F failure;
+  /// Creates a [Failure] instance with the provided failure value [f].
+  Failure(this.f);
+
+  /// The failure value.
+  final F f;
+}
+
+/// Represents an error result with a failure value of type [F], an error object, and a stack trace.
+class Error<S, F> implements Result<S, F> {
+  /// Creates an [Error] instance with the provided failure value [f], error object [e], and stack trace [s].
+  Error(this.f, this.e, this.s);
+
+  /// The failure value.
+  final F f;
+
+  /// The error object associated with the error result.
+  final Object e;
+
+  /// The stack trace associated with the error result.
+  final StackTrace s;
 }
